18211164ef6c67bc23dfc32f7839dbc2a90d7f75
DKosmos <davidakosmos@gmail.com>
2018-03-21 16:06:57 -0700

    App scaffolding done

---
+++ lab-david/src/main.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import './style/main.scss';
+import React from 'react';
+import ReactDom from 'react-dom';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+import Landing from './component/landing';
+import Dashboard from './component/dashboard';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      notes: [],
+    }
+    this.getApp = this.getApp.bind(this);
+  }
+
+  getApp(){
+    return {
+      state: this.state,
+      setState: this.setState.bind(this)
+    }
+  }
+
+  componentDidUpdate() {
+    console.log('__STATE__', this.state);
+  }
+
+  render(){
+    return (
+      <main>
+        <BrowserRouter>
+          <section>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/dashboard' component={() => <Dashboard app={this.getApp()} />} />
+          </section>
+        </BrowserRouter>
+      </main>
+    )
+  }
+}
