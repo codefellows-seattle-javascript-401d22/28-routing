8a1226eddf4a1b06a320c71a078c21ab9dea1f62
Christian Miller <christian.ax.miller@gmail.com>
2018-03-25 20:27:21 -0700

    added cancel button, empty update form

--- lab-christian/src/component/noteitem/noteitem.js
+++ lab-christian/src/component/noteitem/noteitem.js
@@ -1,12 +1,28 @@
 'use strict';
 
 import React from 'react';
+import NoteUpdateForm from './noteupdate/noteupdate';
 
 class NoteItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      mode: 'default'
+    }
 
+    this.handleCancel = this.handleCancel.bind(this);
+    this.switchMode = this.switchMode.bind(this);
     this.onClick = this.onClick.bind(this);
+  }
+
+  handleCancel() {
+    this.switchMode('default');
+  }
+
+  switchMode(mode) {
+    this.setState({
+      mode: mode
+    });
   }
 
   onClick(e) {
@@ -15,13 +31,21 @@ class NoteItem extends React.Component {
   }
 
   render() {
-    return (
-      <li>
-        <div>Title: {this.props.title}</div>
-        <div>Content: {this.props.content}</div>
-        <button onClick={this.onClick}>Delete</button>
-      </li>
-    )
+    if(this.state.mode === 'default') {
+      return(
+        <li>
+          <div>Title: {this.props.title}</div>
+          <div onClick={()=>{this.switchMode('edit');}}>Content: {this.props.content}</div>
+          <button onClick={this.onClick}>Delete</button>
+        </li>
+      )
+    } else if(this.state.mode === 'edit') {
+      return(
+        <li>
+          <NoteUpdateForm handleCancel={this.handleCancel}/>
+        </li>
+      )
+    }
   }
 }
 
---
+++ lab-christian/src/component/noteitem/noteupdate/noteupdate.js
@@ -0,0 +1,64 @@
+'use strict';
+
+import React from 'react';
+import uuidv1 from 'uuid/v1';
+
+class NoteUpdateForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      content: '',
+    }
+
+    this.cancelNote = this.cancelNote.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.onComplete = this.onComplete.bind(this);
+  }
+
+  cancelNote(e) {
+    e.preventDefault();
+    this.props.handleCancel();
+  }
+
+  handleChange(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  onComplete(e) {
+    e.preventDefault();
+    this.props.handleNoteUpdate({
+      title: this.state.title,
+      content: this.state.content,
+    });
+  }
+
+  render() {
+    return (
+      <section id="noteupdate">
+        <form onSubmit={this.onComplete}>
+          <input
+            name='title'
+            type='text'
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+
+          <input
+            name='content'
+            type='text'
+            value={this.state.content}
+            onChange={this.handleChange}
+          />
+
+          <button type='submit'>Add Note</button>
+          <button onClick={this.cancelNote}>Cancel</button>
+        </form>
+      </section>
+    )
+  }
+}
+
+export default NoteUpdateForm;
