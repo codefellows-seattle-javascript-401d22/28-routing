b91705e0bbf36c98c5c56416fe48b28bf23296f1
Christian Miller <christian.ax.miller@gmail.com>
2018-04-15 18:12:54 -0700

    added app component, it was in main.js previously

--- lab-christian/src/main.js
+++ lab-christian/src/main.js
@@ -4,27 +4,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {BrowserRouter, Route} from 'react-router-dom';
 
-import Landing from '../src/component/landing/landing';
-import Dashboard from '../src/component/dashboard/dashboard';
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <main>
-        <BrowserRouter>
-          <section>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/dashboard' component={Dashboard} />
-          </section>
-        </BrowserRouter>
-      </main>
-    )
-  }
-}
-
 ReactDom.render(<App />, document.getElementById('root'));
 export default App;
